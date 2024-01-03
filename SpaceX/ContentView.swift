ed254be7fb5f481824eb1c52aeb6552f8c2b71ed
Mohit Tomer <mstomer31@gmail.com>
2024-01-03 10:05:01 +0530

    Initial Commit

---
+++ SpaceX/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SpaceX
+//
+//  Created by ꧁☆༒☬MoʜɩtToɱɘʀ☬༒☆꧂ on 03/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SpaceX/SpaceXApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceXApp.swift
+//  SpaceX
+//
+//  Created by ꧁☆༒☬MoʜɩtToɱɘʀ☬༒☆꧂ on 03/01/24.
+//
+
+import SwiftUI
+
+@main
+struct SpaceXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
